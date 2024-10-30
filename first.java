a9d07686f1444c84a3916d26ae7c5edb507f08b8
roshin <roshinjamesjohn009@gmail.com>
2024-10-30 14:45:33 +0530

    java1

---
+++ first.java
@@ -0,0 +1,9 @@
+public class first {
+    public int rollno;
+    public first(int roll){
+        rollno=roll;
+    }
+    public void displayRoll(){
+        System.out.println("Roll no. is "+rollno);
+    }
+}
---
+++ second.java
@@ -0,0 +1,6 @@
+public class second {
+    public static void main(String[] args) {
+        first obj=new first(11);
+        obj.displayRoll();
+    }
+}
